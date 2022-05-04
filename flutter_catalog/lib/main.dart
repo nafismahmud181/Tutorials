9b3a62f7794f6447eab8bf487da4f778bad3601d
nafismahmud181 <nafismahmud181@gmail.com>
2022-05-05 02:34:40 +0600

    First Flutter Commit

---
+++ flutter_catalog/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String Hey = "fuck";
+
+    return MaterialApp(home: HomePage());
+  }
+}
